3d1da458e3b4195278f2455316f1d2e7e9af0acc
Nelly Heng <46193167+GitMalice@users.noreply.github.com>
2026-04-22 00:05:14 +0200

    Add files via upload
    set java app base

---
+++ src/main/java/com/demo/App.java
@@ -0,0 +1,11 @@
+package com.demo;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven Demo!");
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
